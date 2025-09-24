93164fdc241d6ff31147c51ba1a0e6405cdff993
Rgedit <angelmiguelparedes@gmail.com>
2025-09-24 19:59:17 -0400

    Colisiones Mejoradas
    - animacion controlada por animationplayer (falta establecerlas con animationTree)
    
    - Limite de Esquinas Mejorado
    
    - Codigo mejorado

--- src/venus/scripts/personajes/Player.gd
+++ src/venus/scripts/personajes/Player.gd
@@ -4,12 +4,7 @@ class_name Player extends Node2D
 var salud: int
 var is_alive: bool #Comprueba si el jugador esta vivo
 var speed: float
-var animacion_actual #Guardara la animacion actual
 #var weapon = Position2D -- por ahora es para guardar la posicion del cañon al disparar
-
-
-func _process(delta):
-	animaciones()
 
 func _physics_process(delta):
 	movimiento()
@@ -18,7 +13,7 @@ func _physics_process(delta):
 func movimiento():
 	pass
 
-func animaciones():
+func animaciones(anim):
 	pass
 
 func ataque():
--- src/venus/scripts/personajes/redship_controller.gd
+++ src/venus/scripts/personajes/redship_controller.gd
@@ -1,24 +1,31 @@
 class_name RedshipController extends Player
 
-onready var animated_sprite = $"../AnimatedSprite"
-onready var redship = $".."
 
+onready var redship = $".."
 
 #movimiento
 var mov
 
 func _ready():
-	animacion_actual = animated_sprite.play("idle")
 	speed = 400
+
+func _process(delta):
+		# Limitar posición dentro de la pantalla
+	var screen_size = get_viewport_rect().size
+	var margin = 20
+
+	redship.position.x = clamp(redship.position.x, 20, screen_size.x - margin)
+	redship.position.y = clamp(redship.position.y, 20, screen_size.y - margin)
 
 func movimiento():
 	mov = Input.get_vector("izquierda", "derecha", "arriba", "abajo")
 	mov.normalized()
 	
+			
 	redship.move_and_slide(mov * speed)
 	
 
-func animaciones():
+func animaciones(anim):
 	pass
 
 func ataque():
