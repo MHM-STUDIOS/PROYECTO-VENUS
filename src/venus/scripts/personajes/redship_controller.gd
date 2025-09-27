60f6a6d1677ff856f15641b7d48bde14d2da2061
Rgedit <angelmiguelparedes@gmail.com>
2025-09-27 16:29:48 -0400

    Movimiento y ejes mejorado
    Se agrego eje mov_x & mov_y
    con esto es mas facil de leer el codigo

--- src/venus/scripts/personajes/redship_controller.gd
+++ src/venus/scripts/personajes/redship_controller.gd
@@ -1,16 +1,20 @@
 class_name RedshipController extends Player
 
+onready var animation_player = $"../AnimationPlayer"
 
-onready var redship = $".."
+onready var redship= $".."
+
 
 #movimiento
+var mov_x
+var mov_y
 var mov
 
 func _ready():
 	speed = 400
 
 func _process(delta):
-		# Limitar posición dentro de la pantalla
+	# Limitar posición dentro de la pantalla
 	var screen_size = get_viewport_rect().size
 	var margin = 20
 
@@ -18,12 +22,12 @@ func _process(delta):
 	redship.position.y = clamp(redship.position.y, 20, screen_size.y - margin)
 
 func movimiento():
-	mov = Input.get_vector("izquierda", "derecha", "arriba", "abajo")
+	mov_x = int(Input.is_action_pressed("derecha")) - int(Input.is_action_pressed("izquierda"))
+	mov_y = int(Input.is_action_pressed("abajo")) - int(Input.is_action_pressed("arriba"))	
+	mov = Vector2(mov_x, mov_y)
 	mov.normalized()
-	
-			
 	redship.move_and_slide(mov * speed)
-	
+
 
 func animaciones(anim):
 	pass
